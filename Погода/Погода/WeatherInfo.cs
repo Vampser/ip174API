077f01c2b580e7622553d259dd0d3cb6cf459bab
Vampser <76053232+Vampser@users.noreply.github.com>
2020-12-16 12:30:16 +0300

    Add files via upload

---
+++ Погода/Погода/WeatherInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Погода.wthr
+{
+    public class WeatherInfo
+    {
+        [JsonProperty("main")]
+        public WeatherMain Main { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
