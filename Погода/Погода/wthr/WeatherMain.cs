80cd1070a039b80bd072998473e64531afb5878b
Vampser <76053232+Vampser@users.noreply.github.com>
2020-12-16 12:31:11 +0300

    Add files via upload

---
+++ Погода/Погода/wthr/WeatherMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Погода.wthr
+{
+    public class WeatherMain
+    {
+
+        [JsonProperty("temp")]
+        public double Temp { get; set; }
+
+        [JsonProperty("feels_like")]
+        public double FeelsLike { get; set; }
+
+        [JsonProperty("temp_min")]
+        public double TempMin { get; set; }
+
+        [JsonProperty("temp_max")]
+        public double TempMax { get; set; }
+
+        [JsonProperty("pressure")]
+        public int Pressure { get; set; }
+
+        [JsonProperty("humidity")]
+        public int Humidity { get; set; }
+
+    }
+}
